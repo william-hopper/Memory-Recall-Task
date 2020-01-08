78dd2bf7df2aca78e5f8a2cdd459de26c253b86e
william-hopper <williamjthopper@gmail.com>
2020-01-08 16:21:26 +0100

    initial

--- exp01_main_script_v01.m
+++ exp01_main_script_v01.m
@@ -16,10 +16,42 @@ clear all; close all; clc; rng('shuffle'); clear mex;
 %% What to do
 % =========================================================================
 
-cfg.do.debug                                   = 0; % should we run in debug mode?
+cfg.do.debug                       = 0;         % should we run in debug mode?
 
-cfg.do.instructions                            = 1; % do we want instructions?
-cfg.do.training_with_feedback                  = 1; % should we play training with feedback
+cfg.do.instructions                = 1;         % do we want instructions?
+cfg.do.training                    = 1;         % should we play training
 
-cfg.do.main_experiment                         = 1; % should we play main_experiment
-cfg.do.reimbursement                           = 1; % should we play reimbursement
+cfg.do.main_experiment             = 1;         % should we play main_experiment
+cfg.do.reimbursement               = 1;         % should we play reimbursement
+
+
+%% set configuration parameters of experiment
+% =========================================================================
+cfg.exp.name                       = 'exp_01';  % name of the experiment
+
+if cfg.do.debug
+else
+end
+
+
+cfg.exp.n_trials                   = 30;        % number of trials
+cfg.exp.baseline_reimbursement     = 15;        % how much money we promise as a baseline
+
+cfg.exp.time.flip_speed            = 0.5;       % number of seconds between flips
+cfg.exp.time.response_speed        = 2;         % number of seconds for participant response
+cfg.exp.time.cnf_time              = 5;         % number of seconds for confidence response
+
+cfg.exp.time.show_ready            = 2;         % how long should ready be shown at the beginning of each trial?
+cfg.exp.time.test_rewatch          = 5;         % how long do participants have to decide to rewatch stimuli?
+cfg.exp.time.show_feedback         = 5;         % how long should the feedback be shown at end of the trial?
+
+
+%% set paths
+% =========================================================================
+addpath(genpath(pwd));  % add all the sub functions
+
+cfg.path.stim                                  = 'Stimuli/'; % link to Stimuli folder
+cfg.path.instructions                          = ([cfg.path.stim,'Instructions/Image_instructions/']);
+cfg.path.training                              = '../../../../ownCloud/CONFPERF_EXP_02/raw/training/';  %'raw/training/';  % path to save main exp data
+cfg.path.main_exp                              = '../../../../ownCloud/CONFPERF_EXP_02/raw/main_exp/';  % 'raw/main_exp/';  % path to save main exp data
+
