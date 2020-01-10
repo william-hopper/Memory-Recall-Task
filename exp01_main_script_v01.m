6a35ac1666ed850271d32fb4c4747f413c8e211c
william-hopper <williamjthopper@gmail.com>
2020-01-10 10:56:25 +0100

    Update exp01_main_script_v01.m

--- exp01_main_script_v01.m
+++ exp01_main_script_v01.m
@@ -5,6 +5,7 @@
 % Composed of:
 
 % Scripts required:
+% make sure to specify path directory
 
 % Author: William Hopper 08/01/2020
 
@@ -52,8 +53,8 @@ addpath(genpath(pwd));  % add all the sub functions
 
 cfg.path.stim                                  = 'Stimuli/'; % link to Stimuli folder
 cfg.path.instructions                          = ([cfg.path.stim,'Instructions/Image_instructions/']);
-cfg.path.training                              = 'C:/Users/william.hopper/ownCloud/PhD/Matlab/Memory-Recall-Task/raw';  %'raw/training/';  % path to save raw exp data
-cfg.path.main_exp                              = 'C:/Users/william.hopper/ownCloud/PhD/Matlab/Memory-Recall-Task/main_exp';  % 'raw/main_exp/';  % path to save main exp data
+cfg.path.training                              = '.../.../.../ownCloud/PhD/Matlab/Memory-Recall-Task/raw';  %'raw/training/';  % path to save raw exp data
+cfg.path.main_exp                              = '.../.../.../ownCloud/PhD/Matlab/Memory-Recall-Task/main_exp';  % 'raw/main_exp/';  % path to save main exp data
 
 create_missing_directories(cfg.path);
 
@@ -71,6 +72,53 @@ cfg.hostname        = deblank(cfg.hostname);  % remove wired characters
 
 cfg.start_time      = datestr(now, 30); % start time
 cfg.fname           = sprintf('%02i_%s_%s', cfg.subid, cfg.subname, cfg.start_time);
+
+% create_missing_directories(cfg.path); % need to specify file location!
+
+
+
+%%  Set up psychtoolbox, open screen etc., save to cfg structure
+% =======================================================================
+% Initial parameters
+PsychDefaultSetup(2);
+
+if cfg.do.debug
+    PsychDebugWindowConfiguration;  % added for the testing phase
+end
+
+Screen('Preference', 'SkipSyncTests', 0);
+Screen('Preference','VisualDebugLevel', 0);  % supress start screen
+HideCursor;
+
+cfg.ptb.screens = Screen('Screens'); % open screen
+cfg.ptb.screenNumber = max(cfg.ptb.screens); % counter the number of max screens
+cfg.ptb.white = WhiteIndex(cfg.ptb.screenNumber); % set up white
+cfg.ptb.black = BlackIndex(cfg.ptb.screenNumber); % set up black
+[cfg.ptb.PTBwindow, cfg.ptb.PTBwindowRect] = PsychImaging('Openwindow', cfg.ptb.screenNumber, cfg.ptb.black);
+Screen('BlendFunction', cfg.ptb.PTBwindow, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA'); % Set the blend funciton for the screen
+
+[cfg.ptb.xCenter, cfg.ptb.yCenter] = RectCenter(cfg.ptb.PTBwindowRect); % Get the centre coordinate of the window
+[cfg.ptb.screenXpixels, cfg.ptb.screenYpixels] = Screen('WindowSize', cfg.ptb.PTBwindow); % Get the center coordinate of the window in pixels
+cfg.ptb.ifi = Screen('GetFlipInterval', cfg.ptb.PTBwindow); % Query the frame duration
+cfg.ptb.framerate = Screen('FrameRate', cfg.ptb.PTBwindow); % Sync us and get a time stamp
+cfg.ptb.waitframes = 1; % set up waitframes
+
+% Position parameters
+cfg.ptb.yHigh                          = cfg.ptb.yCenter/2 ;
+cfg.ptb.yBottom                        = 9/5*cfg.ptb.yCenter ;
+
+% Instructions parameters
+load([cfg.path.stim 'text.mat'], 'text');
+cfg.ptb.instructions = text;
+
+% Text parameters
+cfg.ptb.text.size                      = 60; % what should be the size of texts
+cfg.ptb.text.position                  = cfg.ptb.screenYpixels * 0.20; % Set text position
+cfg.ptb.text.Col                       = cfg.ptb.black;
+cfg.ptb.text.Colbis                    = cfg.ptb.white;
+cfg.ptb.text.gray                      = [180 180 180]; % gray
+cfg.ptb.text.ColAlternative            = [50 50 200]; % blue
+cfg.ptb.text.bigFont                   = 25;
 
 
 
