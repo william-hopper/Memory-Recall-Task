769c95d3f5a24a3596f5638bf89a04b109820ffc
william-hopper <williamjthopper@gmail.com>
2020-01-30 14:11:58 +0100

    updated to generate for multiple trials

--- exp01/main_script_subfunctions/generate_pair_perms_v01.m
+++ exp01/main_script_subfunctions/generate_pair_perms_v01.m
@@ -5,11 +5,15 @@ function [pair_perms] = generate_pair_perms_v01(cfg)
 % Author:   William Hopper
 % Original: 15/01/2020
 
+% pre-allocate
 pair_perms = zeros(2, cfg.exp.n_pairs, cfg.exp.n_trials);
 
+% geenrate pairs for nTrials
 for nTrial = 1:cfg.exp.n_trials
     
-    pair_perms(:,:,nTrial) = reshape(randperm(16), 2, cfg.exp.n_pairs);
+    % pair_perms(1,1,nTrial) + pair_perms(2,1,nTrial) = first pair
+    % pair_perms(1,2,nTrial) + pair_perms(2,2,nTrial) = second pair etc..
+    pair_perms(:,:,nTrial) = reshape(randperm(cfg.exp.n_stim), 2, cfg.exp.n_pairs);
     
 end
 
